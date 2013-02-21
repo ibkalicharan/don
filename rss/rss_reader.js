311d6fb5b9a5598e9b6fa6367202e7d597d66ac2
Matthew Summers <matt@wishf.co.uk>
2013-02-21 18:03:45 +0000

    Generalised RSS reader to a function
    If needed, can add CSS id-ing to <li> elements also

--- rss/rss_reader.js
+++ rss/rss_reader.js
@@ -1,4 +1,5 @@
-        $(document).ready(function () {
+// Old Code
+/*        $(document).ready(function () {
             $.get("edinburgh-university-news.xml", function(data) {
                 var $xml = $(data);
                 var rssListItems = [];
@@ -25,3 +26,41 @@
                 $("#rss").eq(0).append(rssListItems.join(""))
             });
         });
+*/
+
+// Generalised RSS reader to a function (requires jQuery)
+// relativeUrl - URL of the xml file to parse as RSS on the server (since cross domain requests aren't allowed)
+// ulId - The CSS ID you want to use to identify an RSS list
+// linkId - The CSS ID you want to use to identify an RSS link
+// Example of use: 
+/*
+ * $(document).ready(getRss("edinburgh-university-news.xml", "#rss", "#rssLink"));
+ */
+
+function getRss(relativeUrl, ulId, linkId) {
+    $.get(relativeUrl, function(data) {
+            var $xml = $(data);
+            var rssListItems = [];
+            var $rss = $(ulId + " ul").eq(0)
+            $xml.find("item").each(function() {
+                var $this = $(this),
+                    item = {
+                        title: $this.find("title").text(),
+                        link: $this.find("link").text(),
+                        description: $this.find("description").text(),
+                        pubDate: $this.find("pubDate").text(),
+                        author: $this.find("author").text()
+                }
+                
+            console.log(item.title);
+            var $innerLink = $("<a/>", {
+                html: item.title,
+                "class": linkId,
+                href: item.link
+            });
+            rssListItems.push("<li>" + $("<div>").append($innerLink.clone()).html() + "</li>");
+        });
+        console.log(rssListItems.join(""))
+        $("#rss").eq(0).append(rssListItems.join(""))
+    });
+}
