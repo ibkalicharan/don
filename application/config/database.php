c14f3da940015944b68ecbbdcdf053acb8877b9e
Connor Stuart <conkerbot@gmail.com>
2013-02-22 14:48:51 +0000

    Updating DB config

--- application/config/database.php
+++ application/config/database.php
@@ -48,9 +48,9 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost:8889';
+$db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'root';
+$db['default']['password'] = 'smdH1967';
 $db['default']['database'] = 'ilw';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
